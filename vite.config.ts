1be52eee53cae3b6c6fc74ea73368e4cc3036eaa
VanessaMthimkulu <vmthimkulu13@gmail.com>
2025-09-02 16:40:06 +0200

    Optimizing code for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   
   return {
     plugins: [react()],
-    base: '/',
+    base: './',
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
